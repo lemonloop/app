779f09d7c71c8b4d62b16d5c4a4904bee785a497
carlama <carlama@student.ethz.ch>
2024-10-21 17:30:48 +0200

    starting sensor functions

--- src/main.c
+++ src/main.c
@@ -14,9 +14,61 @@ LOG_MODULE_REGISTER(main,LOG_LEVEL_DBG);
 // device pointers
 const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(uart0));
 #define SPIOP	SPI_WORD_SET(8) | SPI_TRANSFER_MSB
-struct spi_dt_spec ism303_spispec = SPI_DT_SPEC_GET(DT_NODELABEL(ism330dhcx), SPIOP, 0);
-struct spi_dt_spec lsm330_spispec = SPI_DT_SPEC_GET(DT_NODELABEL(lsm303agr), SPIOP, 0);
+struct spi_dt_spec ism330_spispec = SPI_DT_SPEC_GET(DT_NODELABEL(ism330dhcx), SPIOP, 0);
+struct spi_dt_spec lsm303_spispec = SPI_DT_SPEC_GET(DT_NODELABEL(lsm303agr), SPIOP, 0);
 const struct gpio_dt_spec uart_ctrl = GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE, uartctrl_gpios);
+
+// sensors
+struct lsm_config { // struct to store the sensor configurations
+        uint8_t ctrl1_A;
+        uint8_t ctrl2_A;
+        uint8_t cfg_reg_a_m;
+        uint8_t cfg_reg_b_m;
+        uint8_t cfg_reg_c_m;
+};
+
+struct lsm_data {
+        uint8_t out_t_a;
+        uint16_t out_x_a;
+        uint16_t out_y_a;
+        uint16_t out_z_a;
+        uint16_t outx_m;
+        uint16_t outy_m;
+        uint16_t outz_m;
+};
+
+//read from a register
+static int lsm_read_reg(uint8_t reg, uint8_t *data, uint8_t size){
+        uint8_t tx_buffer = reg;
+	struct spi_buf tx_spi_buf			= {.buf = (void *)&tx_buffer, .len = 1};
+	struct spi_buf_set tx_spi_buf_set 	= {.buffers = &tx_spi_buf, .count = 1};
+	struct spi_buf rx_spi_bufs 			= {.buf = data, .len = size};
+	struct spi_buf_set rx_spi_buf_set	= {.buffers = &rx_spi_bufs, .count = 1};
+
+	int err = spi_transceive_dt(&lsm303_spispec, &tx_spi_buf_set, &rx_spi_buf_set);
+	if (err < 0) {
+		LOG_ERR("spi_transceive_dt() failed, err: %d", err);
+		return err;
+	}
+	return 0;
+}
+
+static int lsm_write_reg(uint8_t reg, uint8_t value){
+	uint8_t tx_buf[] = {(reg & 0x7F), value};	
+	struct spi_buf 		tx_spi_buf 		= {.buf = tx_buf, .len = sizeof(tx_buf)};
+	struct spi_buf_set 	tx_spi_buf_set	= {.buffers = &tx_spi_buf, .count = 1};
+
+	int err = spi_write_dt(&lsm303_spispec, &tx_spi_buf_set);
+	if (err < 0) {
+		LOG_ERR("spi_write_dt() failed, err %d", err);
+		return err;
+	}
+	return 0;
+}
+
+void lsm_calibrationdata(void){
+        return;
+}
 
 // uart
 static uint8_t rx_buf[10] = {0};
